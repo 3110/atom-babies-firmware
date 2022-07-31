019c4eb75eaff4656b0ec0817f166963d8a1a951
SAITO, Tetsuya <saito.tetsuya@gmail.com>
2022-07-31 11:41:56 +0900

    行が長いのを変更

--- generate_user_custom.py
+++ generate_user_custom.py
@@ -5,7 +5,9 @@ firmware_name = env.GetProjectOption("custom_firmware_name")
 firmware_version = env.GetProjectOption('custom_firmware_version')
 firmware_suffix = env.GetProjectOption('custom_firmware_suffix')
 firmware_dir = env.GetProjectOption('custom_firmware_dir')
-firmware_bin = os.path.join(firmware_dir, '%s_%s%s' % (firmware_name, firmware_version, firmware_suffix))
+
+firmware_bin = '%s_%s%s' % (firmware_name, firmware_version, firmware_suffix)
+firmware_path = os.path.join(firmware_dir, firmware_bin)
 
 app_bin = os.path.join('$BUILD_DIR','${PROGNAME}%s' % firmware_suffix)
 
@@ -13,6 +15,10 @@ env.AddCustomTarget(
     name="firmware",
     dependencies=['buildfs'],
     actions=[
-        '"$PYTHONEXE" "$UPLOADER" --chip esp32 merge_bin -o %s %s $ESP32_APP_OFFSET %s' % (firmware_bin, ' '.join([addr + " " + name for addr, name in env['FLASH_EXTRA_IMAGES']]), app_bin)
+        '"$PYTHONEXE" "$UPLOADER" --chip esp32 merge_bin '
+        '-o %s %s $ESP32_APP_OFFSET %s'
+        % (firmware_path, ' '.join([
+            addr + " " + name for addr, name in env['FLASH_EXTRA_IMAGES']
+        ]), app_bin)
     ],
     title="Generate User Custom")
